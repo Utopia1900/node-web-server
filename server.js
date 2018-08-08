fdb48e8a4f7b7ec761be8ac01421f4f3b04520bb
love_ru1900 <cosem_520@163.com>
2018-08-08 10:58:56 +0800

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const hbs = require('hbs')
 const app = express()
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 app.set('view engine', 'hbs')
 app.use((req, res, next) => {
     var now = new Date().toString();
