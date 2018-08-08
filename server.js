5fed358f786ecf66e15c5650af565abca34a9f22
love_ru1900 <cosem_520@163.com>
2018-08-08 11:06:34 +0800

    Update server port code

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ hbs.registerHelper('screamIt', text => {
     return text.toUpperCase()
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}.`)
 })
 
